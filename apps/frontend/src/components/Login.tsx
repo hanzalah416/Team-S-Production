1e6b881378253e909758815d291c3d63a7fed30e
NerdiCat2 <cdhunt@wpi.edu>
2024-03-30 22:38:44 -0400

    Aligned everything to the left third

--- apps/frontend/src/components/Login.tsx
+++ apps/frontend/src/components/Login.tsx
@@ -22,52 +22,58 @@ const Login: React.FC = () => {
 
 
     return (
-        <div >
-            <h2 className={styles.title}>Sign in</h2>
-            <form>
-                <div className={styles.formGroup}>
-                    <input
-                        className={styles.input}
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        placeholder="Username"
-                    />
-                </div>
+        <div className={styles.container}>
+            <div className={styles.signInForm}>
+                <h2 className={styles.title}>Sign in</h2>
+                <form>
+                    <div className={styles.formGroup}>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            placeholder="Username"
+                        />
+                    </div>
 
-                <div className={styles.formGroup}>
-                    <input
-                        className={styles.input}
-                        type="text"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password"
-                    />
-                </div>
+                    <div className={styles.formGroup}>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password"
+                        />
+                    </div>
+
+                    <div className={styles.extras}>
+                        <div className={styles.cb}>
+                            <input type="checkbox"/>
+                            <label className={styles.remember}>REMEMBER ME</label>
+                        </div>
+
+                        <p className={styles.forgotPassword}>
+                            <a className={styles.forgotLink} href="url">FORGOT PASSWORD?</a>
+                        </p>
+                    </div>
+                </form>
 
 
-                <div className= {styles.cb}>
-                    <input type="checkbox"/>
-                    <label className = {styles.remember} >REMEMBER ME</label>
-                </div>
-            </form>
-
-            <p className={styles.h3}>
-                <a className = {styles.forgotLink} href="url">FORGOT PASSWORD?</a>
-            </p>
-
-
-            <div className={styles.buttonGroup}>
                 <div className={styles.buttonGroup}>
+                    <div className={styles.buttonGroup}>
                     <button className={`${styles.button} ${styles.loginButton}`} type="button" onClick={handleLogin}>
-                        Login
-                    </button>
+                            Login
+                        </button>
+                    </div>
+                    <div className={styles.buttonGroup}>
+                        <button className={`${styles.button} ${styles.newAcctButton}`} type="button" onClick = {handleCreateAccount}>
+                            Create Account
+                        </button>
+                    </div>
                 </div>
-                <div className={styles.buttonGroup}>
-                    <button className={`${styles.button} ${styles.newAcctButton}`} type="button" onClick = {handleCreateAccount}>
-                        Create Account
-                    </button>
-                </div>
+            </div>
+            <div className={styles.slideShow}>
+
             </div>
         </div>
     );
