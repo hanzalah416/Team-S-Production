e429103762509850ffdc52505270b8c41d49da23
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2024-03-30 20:19:27 -0400

    Cleaning up code
    Fixed all errors and now we are just waiting for backend to finish

--- apps/backend/src/Readcsv.ts
+++ apps/backend/src/Readcsv.ts
@@ -1,17 +1,15 @@
 import fs from "fs";
 
 function readCSV(filePath: string): Array<Array<string>> {
-    try {
-        const data = fs.readFileSync(filePath, "utf-8")
-        return data
-            .split('\r\n')
-            .slice(1,-1) 
-            .map(line => line.split(','));
-    }
-    catch (error) {
-        console.error(error);
-        return []
-    }
+  try {
+    const data = fs.readFileSync(filePath, "utf-8");
+    return data
+      .split("\r\n")
+      .slice(1, -1)
+      .map((line) => line.split(","));
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 export default readCSV;
-
--- apps/backend/src/routes/getShortestPath.ts
+++ apps/backend/src/routes/getShortestPath.ts
@@ -1,36 +1,42 @@
 import express, { Router, Request, Response } from "express";
-import MakeGraph from "./makegraph";
-import readCSV from 
+import MakeGraph from "../makegraph";
+import readCSV from "../Readcsv.ts";
+/*
 import { Prisma } from "database";
 import PrismaClient from "../bin/database-connection.ts";
+ */
 
 const router: Router = express.Router();
 
 router.get("/pathfind", async function (req: Request, res: Response) {
-    const graph = new Graph();
+  try {
+    const graph = new MakeGraph();
 
-  const nodes = await readCSV('L1Nodes.csv');
-  const edges = await readCSV('L1Edges.csv');
+    const nodes = readCSV("L1Nodes.csv");
+    const edges = readCSV("L1Edges.csv");
 
     // Add all nodes to graph
-    nodes.forEach(nodeProperties => {
-        graph.addNode(nodeProperties[0]);
+    nodes.forEach((nodeProperties) => {
+      graph.addNode(nodeProperties[0]);
     });
 
     // Add all edges to graph
-    edges.forEach(edge => {
-        graph.addEdge(edge[0], edge[1]);
+    edges.forEach((edge) => {
+      graph.addEdge(edge[0], edge[1]);
     });
 
-     //pathfinding using BFS
+    //pathfinding using BFS
     const { startNodeID, endNodeID } = req.body;
-    const path: string[] = graph.BFS(startNodeID, endNodeID);
-
+    const path = graph.BFS(startNodeID, endNodeID);
+    if (!path) {
+      throw new Error("Path was undefined");
+    }
     // Check
     path.length === 0 ? res.sendStatus(204) : res.json({ pathNodeIDs: path });
   } catch (error) {
     console.error("Error by pathfinding: ", error);
+    res.sendStatus(400).send("Path was undefined");
   }
-);
+});
 
 export default router;
