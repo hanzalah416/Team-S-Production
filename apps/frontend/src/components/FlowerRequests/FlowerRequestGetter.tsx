34ecd3153d9cc4f6cdc2fe581ce0b20390c341f5
Dorothy Alexander <dcalexander2004@gmail.com>
2024-04-03 05:00:41 -0400

    All Service Requests List

--- apps/frontend/src/components/FlowerRequests/FlowerRequestGetter.tsx
+++ apps/frontend/src/components/FlowerRequests/FlowerRequestGetter.tsx
@@ -2,6 +2,25 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { FlowerRequestDisplay } from "./FlowerRequestDisplay.tsx";
 import { flowerform } from "../common/flowerform.ts";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
 
 export function FlowerRequestGetter() {
   const [flowerRequestData, setFlowerRequestData] = useState<flowerform[]>([]);
@@ -18,17 +37,31 @@ export function FlowerRequestGetter() {
 
   return (
     <div className="flex flex-colgap-5">
-      {flowerRequestData != undefined ? (
-        flowerRequestData.map((flowerform) => {
-          return (
-            <FlowerRequestDisplay
-              flowerform={flowerform}
-            ></FlowerRequestDisplay>
-          );
-        })
-      ) : (
-        <>no</>
-      )}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Patient Name</StyledTableCell>
+              <StyledTableCell align="right">Patient Room</StyledTableCell>
+              <StyledTableCell align="right">Custom Message</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {flowerRequestData != undefined ? (
+                flowerRequestData.map((flowerform) => {
+                  return (
+                      <FlowerRequestDisplay
+                          flowerform={flowerform}
+                      ></FlowerRequestDisplay>
+                  );
+                })
+            ) : (
+                <>no</>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
     </div>
   );
 }
