3df35705fd0ee24a4372fa605d1d07820b3e20c4
NerdiCat2 <cdhunt@wpi.edu>
2024-04-03 22:21:38 -0400

    Filling table but not printing

--- apps/frontend/src/components/NodeDataPage.tsx
+++ apps/frontend/src/components/NodeDataPage.tsx
@@ -1,5 +1,5 @@
 //import * as React from 'react';\
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -10,46 +10,56 @@ import Paper from "@mui/material/Paper";
 import axios from "axios";
 //import CreateCSVFromDB from "../../../backend/src/DBtoCSV.ts";
 
-function createData(
-  nodeID: string,
-  xcoord: number,
-  ycoord: number,
-  floor: string,
-  building: string,
-  nodeType: string,
-  longName: string,
-  shortName: string,
-) {
-  return {
-    nodeID,
-    xcoord,
-    ycoord,
-    floor,
-    building,
-    nodeType,
-    longName,
-    shortName,
-  };
+// function createData(
+//   nodeID: string,
+//   xcoord: number,
+//   ycoord: number,
+//   // floor: string,
+//   // building: string,
+//   // nodeType: string,
+//   longName: string,
+//   //shortName: string,
+// ) {
+//   return {
+//     nodeID,
+//     xcoord,
+//     ycoord,
+//       /*
+//     floor,
+//     building,
+//     nodeType,
+//        */
+//     longName,
+//     //shortName,
+//   };
+// }
+
+interface NodeRow {
+  nodeID: string;
+  xcoord: number;
+  ycoord: number;
+  longName: string;
 }
 
-const rows = [
-  createData(
-    "CCONF001L1",
-    2255,
-    849,
-    "L1",
-    "45 Francis",
-    "CONF",
-    "Anesthesia Conf Floor L1",
-    "Conf C001L1",
-  ),
-  createData("Ice cream sandwich", 237, 9.0, "24", "4.0", "0", "", ""),
-  createData("Eclair", 262, 16.0, "24", "4.0", "0", "", ""),
-  createData("Cupcake", 305, 3.7, "24", "4.0", "0", "", ""),
-  createData("Gingerbread", 356, 16.0, "24", "4.0", "0", "", ""),
-];
+// const rows = [
+//   createData(
+//     "CCONF001L1",
+//     2255,
+//     849,
+//     "L1",
+//     "45 Francis",
+//     "CONF",
+//     "Anesthesia Conf Floor L1",
+//     "Conf C001L1",
+//   ),
+//   createData("Ice cream sandwich", 237, 9.0, "24", "4.0", "0", "", ""),
+//   createData("Eclair", 262, 16.0, "24", "4.0", "0", "", ""),
+//   createData("Cupcake", 305, 3.7, "24", "4.0", "0", "", ""),
+//   createData("Gingerbread", 356, 16.0, "24", "4.0", "0", "", ""),
+// ];
 
 const NodeDataPage: React.FC = () => {
+  const [rows, setRows] = useState<NodeRow[]>([]);
   useEffect(() => {
     // Run main function when the component mounts
     {
@@ -59,6 +69,10 @@ const NodeDataPage: React.FC = () => {
       const res = await axios.get("/api/nodes");
       console.log(res.data);
       console.log("successfully got data from get request");
+      setRows(res.data);
+      // res.data.forEach((row: NodeRow) => {
+      //     createData(row.nodeID,row.xcoord,row.ycoord,row.longName);
+      //   });
     }
     fetchData().then();
   }, []);
@@ -70,11 +84,11 @@ const NodeDataPage: React.FC = () => {
             <TableCell>nodeID(key)</TableCell>
             <TableCell align="center">xcoord</TableCell>
             <TableCell align="center">ycoord</TableCell>
-            <TableCell align="center">floor</TableCell>
-            <TableCell align="center">building</TableCell>
-            <TableCell align="center">nodeType</TableCell>
+            {/*<TableCell align="center">floor</TableCell>*/}
+            {/*<TableCell align="center">building</TableCell>*/}
+            {/*<TableCell align="center">nodeType</TableCell>*/}
             <TableCell align="center">longName</TableCell>
-            <TableCell align="center">shortName</TableCell>
+            {/*<TableCell align="center">shortName</TableCell>*/}
           </TableRow>
         </TableHead>
         <TableBody>
@@ -88,11 +102,11 @@ const NodeDataPage: React.FC = () => {
               </TableCell>
               <TableCell align="right">{row.xcoord}</TableCell>
               <TableCell align="right">{row.ycoord}</TableCell>
-              <TableCell align="right">{row.floor}</TableCell>
-              <TableCell align="right">{row.building}</TableCell>
-              <TableCell align="right">{row.nodeType}</TableCell>
+              {/*<TableCell align="right">{row.floor}</TableCell>*/}
+              {/*<TableCell align="right">{row.building}</TableCell>*/}
+              {/*<TableCell align="right">{row.nodeType}</TableCell>*/}
               <TableCell align="right">{row.longName}</TableCell>
-              <TableCell align="right">{row.shortName}</TableCell>
+              {/*<TableCell align="right">{row.shortName}</TableCell>*/}
             </TableRow>
           ))}
         </TableBody>
