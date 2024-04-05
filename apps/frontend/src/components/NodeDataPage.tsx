b01b087399bb03dbd86c71ba840a42eb1813134a
NateSchneid <natemschneider@gmail.com>
2024-04-05 00:36:30 -0400

    display for csv import/export

--- apps/frontend/src/components/NodeDataPage.tsx
+++ apps/frontend/src/components/NodeDataPage.tsx
@@ -1,6 +1,6 @@
 //import * as React from 'react';\
 import styles from './NodeDataPage.module.css';
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
@@ -12,6 +12,11 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import axios from "axios";
 
 const VisuallyHiddenInput = styled('input')({
@@ -46,72 +51,119 @@ const NodeDataPage: React.FC = () => {
     fetchData().then();
   }, []);
   console.log(rows);
+
+    const [value, setValue] = React.useState('1');
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
+
   return (
       <div className={styles.outerDiv}>
-              <div className={styles.nodeCSV}>
-                  <h2>Import / Export Nodes</h2>
-                  <div className={styles.Butt}>
-                      <div className={styles.ospacer}></div>
-                      <Button
-                          className={styles.ufileButton}
-                          component="label"
-                          role={undefined}
-                          variant="contained"
-                          tabIndex={-1}
-                          startIcon={<CloudUploadIcon/>}
-                          style={{
-                              backgroundColor: "#003b9c",
-                              fontFamily: "Poppins",
-                              fontSize: 14,
-                              textAlign: "center",
-                          }}>Import Nodes<VisuallyHiddenInput type="file"/>
-                      </Button>
-                      <div className={styles.spacer}></div>
-                      <Button
-                          className={styles.dfileButton}
-                          component="label"
-                          role={undefined}
-                          variant="contained"
-                          tabIndex={-1}
-                          startIcon={<CloudDownloadIcon/>}
-                          style={{
-                              backgroundColor: "#003b9c",
-                              fontFamily: "Poppins",
-                              fontSize: 14,
-                              textAlign: "center",
-                          }}>Export Nodes<VisuallyHiddenInput type="file"/>
-                      </Button>
-                      <div className={styles.ospacer}></div>
-                  </div>
-              </div>
-
-          <div className={styles.tableDiv}>
-              <TableContainer component={Paper} style={{marginTop: "75px"}}>
-                  <Table sx={{minWidth: 650}} aria-label="simple table">
-                      <TableHead>
-                          <TableRow>
-                              <TableCell>nodeID(key)</TableCell>
-                              <TableCell align="center">xcoord</TableCell>
-                              <TableCell align="center">ycoord</TableCell>
-                              <TableCell align="center">longName</TableCell>
-                          </TableRow>
-                      </TableHead>
-                      <TableBody>
-                          {rows.map((row) => (
-                              <TableRow
-                                  key={row.id}
-                                  sx={{"&:last-child td, &:last-child th": {border: 0}}}
-                              >
-                                  <TableCell align="right">{row.id}</TableCell>
-                                  <TableCell align="right">{row.xcoord}</TableCell>
-                                  <TableCell align="right">{row.ycoord}</TableCell>
-                                  <TableCell align="right">{row.longName}</TableCell>
-                              </TableRow>
-                          ))}
-                      </TableBody>
-                  </Table>
-              </TableContainer>
-          </div>
+          <Box sx={{ width: '100%', typography: 'body1' }}>
+              <TabContext value={value}>
+                  <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                      <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
+                          <Tab label="Nodes" value="1" />
+                          <Tab label="Edges" value="2" />
+                      </TabList>
+                  </Box>
+                  <TabPanel value="1" style={{padding: 0 }}>
+                      <div className={styles.nodeCSV}>
+                          { /* <h2>Import / Export Nodes</h2> */}
+                              <Button
+                                  className={styles.ufileButton}
+                                  component="label"
+                                  role={undefined}
+                                  variant="contained"
+                                  tabIndex={-1}
+                                  startIcon={<CloudUploadIcon/>}
+                                  style={{
+                                      marginRight: "5px",
+                                      backgroundColor: "#003b9c",
+                                      fontFamily: "Poppins",
+                                      fontSize: 14,
+                                      textAlign: "center",
+                                  }}>Import Nodes<VisuallyHiddenInput type="file"/>
+                              </Button>
+                              <Button
+                                  className={styles.dfileButton}
+                                  component="label"
+                                  role={undefined}
+                                  variant="contained"
+                                  tabIndex={-1}
+                                  startIcon={<CloudDownloadIcon/>}
+                                  style={{
+                                      marginLeft: "5px",
+                                      backgroundColor: "#003b9c",
+                                      fontFamily: "Poppins",
+                                      fontSize: 14,
+                                      textAlign: "center",
+                                  }}>Export Nodes<VisuallyHiddenInput type="file"/>
+                              </Button>
+                      </div>
+                          <TableContainer component={Paper} style={{marginTop: "75px"}}>
+                              <Table sx={{minWidth: 650}} aria-label="simple table">
+                                  <TableHead>
+                                      <TableRow>
+                                          <TableCell>nodeID(key)</TableCell>
+                                          <TableCell align="center">xcoord</TableCell>
+                                          <TableCell align="center">ycoord</TableCell>
+                                          <TableCell align="center">longName</TableCell>
+                                      </TableRow>
+                                  </TableHead>
+                                  <TableBody>
+                                      {rows.map((row) => (
+                                          <TableRow
+                                              key={row.id}
+                                              sx={{"&:last-child td, &:last-child th": {border: 0}}}
+                                          >
+                                              <TableCell align="right">{row.id}</TableCell>
+                                              <TableCell align="right">{row.xcoord}</TableCell>
+                                              <TableCell align="right">{row.ycoord}</TableCell>
+                                              <TableCell align="right">{row.longName}</TableCell>
+                                          </TableRow>
+                                      ))}
+                                  </TableBody>
+                              </Table>
+                          </TableContainer>
+                  </TabPanel>
+                  <TabPanel value="2" style={{padding: 0}}>
+                      <div className={styles.nodeCSV}>
+                          { /* <h2>Import / Export Nodes</h2> */}
+                              <Button
+                                  className={styles.ufileButton}
+                                  component="label"
+                                  role={undefined}
+                                  variant="contained"
+                                  tabIndex={-1}
+                                  startIcon={<CloudUploadIcon/>}
+                                  style={{
+                                      marginRight: "5px",
+                                      backgroundColor: "#003b9c",
+                                      fontFamily: "Poppins",
+                                      fontSize: 14,
+                                      textAlign: "center",
+                                  }}>Import Edges<VisuallyHiddenInput type="file"/>
+                              </Button>
+                              <Button
+                                  className={styles.dfileButton}
+                                  component="label"
+                                  role={undefined}
+                                  variant="contained"
+                                  tabIndex={-1}
+                                  startIcon={<CloudDownloadIcon/>}
+                                  style={{
+                                      marginLeft: "5px",
+                                      backgroundColor: "#003b9c",
+                                      fontFamily: "Poppins",
+                                      fontSize: 14,
+                                      textAlign: "center",
+                                  }}>Export Edges<VisuallyHiddenInput type="file"/>
+                              </Button>
+                      </div>
+                  </TabPanel>
+              </TabContext>
+          </Box>
       </div>
   );
 };
