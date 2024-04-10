65182c2ab1297771e38af7cbb018898017f704de
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2024-04-09 22:18:53 -0400

    removed security

--- apps/frontend/src/components/flower_requests/FlowerRequestGetter.tsx
+++ apps/frontend/src/components/flower_requests/FlowerRequestGetter.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { FlowerRequestDisplay } from "./FlowerRequestDisplay.tsx";
-import { securityform } from "../common/flowerform.ts";
+import { flowerform } from "../common/flowerform.ts";
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -22,7 +22,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 export function FlowerRequestGetter() {
-  const [flowerRequestData, setFlowerRequestData] = useState<securityform[]>([]);
+  const [flowerRequestData, setFlowerRequestData] = useState<flowerform[]>([]);
 
   useEffect(() => {
     async function fetchData() {
