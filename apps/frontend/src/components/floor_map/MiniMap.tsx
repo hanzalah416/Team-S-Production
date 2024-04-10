686f7aba08188f9608b8890107630664370ec126
NateSchneid <natemschneider@gmail.com>
2024-04-09 22:26:07 -0400

    done with testing

--- apps/frontend/src/components/floor_map/MiniMap.tsx
+++ apps/frontend/src/components/floor_map/MiniMap.tsx
@@ -8,7 +8,7 @@ import f3 from "../assets/HospitalMap/03_thethirdfloor.png";
 import styles from "./MiniMap.module.css";
 
 function MiniMap() {
-    return (
+  return (
     // <div className={styles.outerDiv}>
     //   <img src={ll1} alt="map" className={styles.ll1} />
     //   <img src={ll2} alt="map" className={styles.ll2} />
@@ -16,32 +16,31 @@ function MiniMap() {
     //   <img src={f2} alt="map" className={styles.f2} />
     //   <img src={f3} alt="map" className={styles.f3} />
     // </div>
-<div className={styles.outerOuterDiv}>
-        <div className={styles.outerDiv}>
+    <div className={styles.outerOuterDiv}>
+      <div className={styles.outerDiv}>
         <div className={styles.ll1}>
-            <header>LL1</header>
-            <img src={ll1} alt="map"/>
+          <header>LL1</header>
+          <img src={ll1} alt="map" />
         </div>
-    <div className={styles.ll2}>
-        <header>LL2</header>
-        <img src={ll2} alt="map"/>
+        <div className={styles.ll2}>
+          <header>LL2</header>
+          <img src={ll2} alt="map" />
+        </div>
+        <div className={styles.f1}>
+          <header>1</header>
+          <img src={f1} alt="map" />
+        </div>
+        <div className={styles.f2}>
+          <header>2</header>
+          <img src={f2} alt="map" />
+        </div>
+        <div className={styles.f3}>
+          <header>3</header>
+          <img src={f3} alt="map" />
+        </div>
+      </div>
     </div>
-    <div className={styles.f1}>
-        <header>1</header>
-        <img src={f1} alt="map"/>
-    </div>
-    <div className={styles.f2}>
-        <header>2</header>
-        <img src={f2} alt="map"/>
-    </div>
-    <div className={styles.f3}>
-        <header>3</header>
-        <img src={f3} alt="map"/>
-    </div>
-    </div>
-</div>
-    )
-        ;
+  );
 }
 
 export default MiniMap;
