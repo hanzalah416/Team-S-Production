06111e562a4627d47f1e74689f43205a717bd911
KimAllise32 <kacummings@wpi.edu>
2024-04-14 22:29:18 -0400

    attempt at medicine csv

--- apps/backend/src/seed.ts
+++ apps/backend/src/seed.ts
@@ -1,12 +1,15 @@
 import PrismaClient from "./bin/database-connection.ts";
 
 import readCSVFile from "./Readcsv.ts";
+import * as console from "console";
 
 async function seed() {
   const edges = readCSVFile("L1Edges.csv");
   const nodes = readCSVFile("L1Nodes.csv");
+  //const medicines = readCSVFile("Medicine.csv");
   const dbNodeEdges = await PrismaClient.nodeEdge.findMany();
   const dbNodes = await PrismaClient.node.findMany();
+  //const medicineArray = await PrismaClient.medicine.findMany();
   if (dbNodes.length == 0) {
     for (const node of nodes) {
       await PrismaClient.node.upsert({
@@ -39,6 +42,19 @@ async function seed() {
     }
     console.log("Edges populated");
   }
+
+  // if (medicineArray.length == 0) {
+  //   for (const medicine of medicines) {
+  //     await PrismaClient.medicine.create({
+  //       data: {
+  //         CAS: medicine[1],
+  //         genericName: medicine[0],
+  //         synName: medicine[2],
+  //       },
+  //     });
+  //   }
+  //   console.log("Medicine Populated");
+  // }
 
   const temp = await PrismaClient.hospitalUser.findMany({
     where: {
