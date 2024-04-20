4622300e413412191f2a79b0082fce4602ec38ee
Jeffrey Li <jeffrey.cui.li@gmail.com>
2024-04-20 18:06:59 -0400

    Heropage 1/2

--- apps/frontend/src/components/login/Login.tsx
+++ apps/frontend/src/components/login/Login.tsx
@@ -29,12 +29,12 @@ const Login: React.FC = () => {
                 <h1 className={styles.heading}>Brigham and Women's Hospital</h1>
                 <br/>
                 <br/>
-                <br/>
+
                 <p className={styles.heading2}>Helping our patients and their families get back to what matter most</p>
                 <br/>
                 <br/>
                 <br/>
-                <br/>
+
 
                 <Button onClick={navigateMap} color="primary" style={{
                     backgroundImage: `url(${mapImage})`,
