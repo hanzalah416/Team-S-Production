d085a5608afc773ce60b7d21a3b68a3c38ee9cc1
Jeffrey Li <jeffrey.cui.li@gmail.com>
2024-04-20 02:21:49 -0400

    Heropage 1/2

--- apps/frontend/src/components/login/Login.tsx
+++ apps/frontend/src/components/login/Login.tsx
@@ -1,49 +1,60 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState} from "react";
 import styles from "./Login.module.css";
-import heroImage from "../assets/HeroPhotos/Temp1.png";
-import heroImage2 from "../assets/HeroPhotos/nurse6.jpg";
-import heroImage3 from "../assets/HeroPhotos/Temp3.png";
-import bwhLogo from "../assets/bwh-logo.svg";
+import heroImage from "../assets/HeroPhotos/heroImage.png";
+import mapImage from "../assets/mapImage.png";
+import Button from "@mui/material/Button";
+// import bwhLogo from "../assets/bwh-logo.svg";
+import {useNavigate} from "react-router-dom";
+import {Interests} from "@mui/icons-material";
+
+
+
+
 const Login: React.FC = () => {
-
-    // const navigate = useNavigate();
-  const [backgroundImage, setBackgroundImage] = useState<string>(heroImage);
-
-  useEffect(() => {
-    const imageUrls = [heroImage, heroImage2, heroImage3];
-    const randomImageUrl =
-      imageUrls[Math.floor(imageUrls.length * Math.random())];
-    setBackgroundImage(`url(${randomImageUrl})`);
-  }, []);
+    const navigate = useNavigate();
+    const navigateMap = () => {
+        navigate("/floor-map");
+    };
 
 
-  return (
+    return (
 
-    <div >
-      <div className={styles.wholePage}>
         <div className={styles.container}>
-          <form className={styles.signInForm}>
-            <img src={bwhLogo} className={styles.logo} alt={"BWH logo"} />
-            <div className={styles.buttonGroup}>
-              <div className={styles.buttonGroup}>
-
-
-              </div>
+            <div className={styles.imageContainer}>
+                <img src={heroImage} alt="Covering 3/4 page" className={styles.coverImage}/>
+                <div className={styles.textOverlay}>
+                    <p className={styles.disclaimer}>This website is a term project exercise for WPI CS 3733 Software
+                        Engineering (Prof. Wong) and is not to be confused with the actual Brigham & Women’s
+                        Hospital website</p>
+                </div>
             </div>
-          </form>
-          <div
-            className={styles.slideShow}
-            style={{ backgroundImage: backgroundImage }}
-          >
-            {/*<img src="./src/components/assets/HeroPhotos/Temp1.png" className={"heroImage"} alt={"map"}/>*/}
-          </div>
+            <div className={styles.textContainer}>
+                <h1 className={styles.heading}>Brigham and Women's Hospital</h1>
+                <br/>
+            <br/>
+                <br/>
+                <p className={styles.heading2}>Helping our patients and their families get back to what matter most</p>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+                <Button className={styles.position} onClick={navigateMap} color="primary" style={{
+                    backgroundImage: `url(${mapImage})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    width: '200px', // Adjust button width as needed
+                    height: '200px', // Adjust button height as needed
+
+                }}
+                >
+                <span style={{color: 'black', fontSize: '18px', fontStyle: Interests, marginTop: '150px'}}>
+                View Map</span>
+                </Button>
+            </div>
         </div>
-      </div>
-    </div>
-
-
-
-  );
+);
 };
+
 
 export default Login;
