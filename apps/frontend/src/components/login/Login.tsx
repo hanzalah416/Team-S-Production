a4f3ac3a7811396493d3f3ac124147ddad52c9c2
Jeffrey Li <jeffrey.cui.li@gmail.com>
2024-04-21 00:45:52 -0400

    Heropage 1/2

--- apps/frontend/src/components/login/Login.tsx
+++ apps/frontend/src/components/login/Login.tsx
@@ -1,7 +1,7 @@
 // import React, { useState} from "react";
 import styles from "./Login.module.css";
 import heroImage from "../assets/HeroPhotos/heroImage.png";
-import mapImage from "../assets/mapImage.png";
+import mapImage from "../assets/HeroPhotos/mapImage.png";
 import Button from "@mui/material/Button";
 // import bwhLogo from "../assets/bwh-logo.svg";
 import {useNavigate} from "react-router-dom";
@@ -13,20 +13,27 @@ const Login: React.FC = () => {
         navigate("/floor-map");
     };
 
-
     return (
 <div>
         <div className={styles.container}>
             <div className={styles.imageContainer}>
                 <img src={heroImage} alt="Covering 3/4 page" className={styles.coverImage}/>
+                <div className = {styles.backBottom}>
+<br/>
+                    <br/>
+                    <br/>
+
+                </div>
+
                 <div className={styles.textOverlay}>
                     <p className={styles.disclaimer}>This website is a term project exercise for WPI CS 3733 Software
                         Engineering (Prof. Wong) and is not to be confused with the actual Brigham & Women’s
                         Hospital website</p>
+
                 </div>
             </div>
             <div className={styles.textContainer}>
-                <br/><br/>
+            <br/><br/>
                 <h1 className={styles.heading}>Brigham and Women's Hospital</h1>
 
                 <br/>
@@ -42,60 +49,14 @@ const Login: React.FC = () => {
                     backgroundPosition: 'center',
                     width: '200px', // Adjust button width as needed
                     height: '200px', // Adjust button height as needed
-
+                    marginLeft: '100px',
+                    marginRight: '100px',
                 }}
                 >
                 <span style={{color: 'black', fontSize: '18px', marginTop: '150px'}}>
                 View Map</span>
                 </Button>
             </div>
-        </div>
-
-    <div className = {styles.bottomHalf}>
-        <div className = {styles.firstHalfBottom}>
-        <br/>
-        <Button onClick={navigateMap} color="primary" style={{
-            backgroundImage: `url(${mapImage})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            width: '345px', // Adjust button width as needed
-            height: '345px', // Adjust button height as needed
-
-        }}
-        >
-
-                <span style={{color: 'black', fontSize: '18px', marginTop: '150px'}}>
-                View Map</span>
-        </Button>
-        <Button onClick={navigateMap} color="primary" style={{
-            backgroundImage: `url(${mapImage})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            width: '345px', // Adjust button width as needed
-            height: '345px', // Adjust button height as needed
-
-        }}
-        >
-                <span style={{color: 'black', fontSize: '18px', marginTop: '150px'}}>
-                View Map</span>
-        </Button>
-        <Button onClick={navigateMap} color="primary" style={{
-            backgroundImage: `url(${mapImage})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            width: '345px', // Adjust button width as needed
-            height: '345px', // Adjust button height as needed
-        }}
-        >
-                <span style={{color: 'black', fontSize: '18px', marginTop: '150px'}}>
-                View Map</span>
-        </Button>
-        </div>
-
-        <div className = {styles.secondHalfBottom}>
-
-
-
         </div>
 
 
@@ -108,8 +69,6 @@ const Login: React.FC = () => {
 
 
 
-
-</div>
     );
 };
 
