478bcc5740830eb85abf0cde21c1c28c209dbfaa
Hanzalah Qamar <hanzalah.qamar02@gmail.com>
2024-04-23 20:17:32 -0400

    merged main into keyselection

--- apps/frontend/src/components/login/Login.tsx
+++ apps/frontend/src/components/login/Login.tsx
@@ -11,80 +11,82 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const Login: React.FC = () => {
-  const navigate = useNavigate();
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const navigateMap = () => {
-    navigate("/floor-map");
-  };
-  const images = [heroImage, heroImage1, heroImage2, heroImage3];
+    const navigate = useNavigate();
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const navigateMap = () => {
+        navigate("/floor-map");
+    };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // Update the index to display the next image
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000); // Change the duration here (in milliseconds)
+    const images = [heroImage, heroImage1, heroImage2, heroImage3];
 
-    return () => clearInterval(interval); // Cleanup the interval on component unmount
-  }, [images.length]); // Empty dependency array to run this effect only once on component mount
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // Update the index to display the next image
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 5000); // Change the duration here (in milliseconds)
 
-  return (
-    <div>
-      <div className={styles.container}>
-        <div className={styles.imageContainer}>
-          <div className={styles.slideshow}>
-            {images.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt={`Slide ${index}`}
-                className={styles.coverImage}
-                style={{
-                  display: index === currentImageIndex ? "block" : "none",
-                  transition: "opacity 2s ease-in",
-                }}
-              />
-            ))}
-          </div>
+        return () => clearInterval(interval); // Cleanup the interval on component unmount
+    }, [images.length]); // Empty dependency array to run this effect only once on component mount
 
-          <div className={styles.textOverlay}>
-            <p className={styles.disclaimer}>
-              This website is a term project exercise for WPI CS 3733 Software
-              Engineering (Prof. Wong) and is not to be confused with the actual
-              Brigham & Women’s Hospital website
-            </p>
-          </div>
-        </div>
-        <div className={styles.textContainer}>
-          <br />
-          <br />
-          <h1 className={styles.heading}>Brigham and Women's Hospital</h1>
-          <br />
-          <p className={styles.heading2}>
-            Helping our patients and their families get back to what matters
-            most.
-          </p>
-          <br />
-          <Button
-            onClick={navigateMap}
-            color="primary"
-            style={{
-              backgroundImage: `url(${mapImage})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              width: "200px", // Adjust button width as needed
-              height: "200px", // Adjust button height as needed
-            }}
-          >
+    return (
+        <div>
+            <div className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <div className={styles.slideshow}>
+                        {images.map((image, index) => (
+                            <img
+                                key={index}
+                                src={image}
+                                alt={`Slide ${index}`}
+                                className={styles.coverImage}
+                                style={{
+                                    display: index === currentImageIndex ? "block" : "none",
+                                    transition: "opacity 2s ease-in",
+                                }}
+                            />
+                        ))}
+                    </div>
+
+                    <div className={styles.textOverlay}>
+                        <p className={styles.disclaimer}>
+                            This website is a term project exercise for WPI CS 3733 Software
+                            Engineering (Prof. Wong) and is not to be confused with the actual
+                            Brigham & Women’s Hospital website
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.textContainer}>
+                    <br />
+                    <br />
+                    <h1 className={styles.heading}>Brigham and Women's Hospital</h1>
+                    <br />
+                    <p className={styles.heading2}>
+                        Helping our patients and their families get back to what matters
+                        most.
+                    </p>
+                    <br />
+                    <Button
+                        onClick={navigateMap}
+                        color="primary"
+                        style={{
+                            backgroundImage: `url(${mapImage})`,
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                            width: "200px", // Adjust button width as needed
+                            height: "200px", // Adjust button height as needed
+                        }}
+                    >
             <span
-              style={{ color: "black", fontSize: "18px", marginTop: "150px" }}
+                style={{ color: "black", fontSize: "18px", marginTop: "150px" }}
             >
               View Map
             </span>
-          </Button>
+                    </Button>
+
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
