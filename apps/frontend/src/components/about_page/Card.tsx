57b9cfe8daa14e0f4e6faa60790d90683b501479
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2024-04-23 23:30:42 -0400

    prettier fixes

--- apps/frontend/src/components/about_page/Card.tsx
+++ apps/frontend/src/components/about_page/Card.tsx
@@ -6,26 +6,35 @@ interface ProjectCardProps {
   label: string;
 }
 
-
-const ProjectCard: React.FC<ProjectCardProps> = ({description, imgUrl, label}) => {
-    return (
-
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-            <Tooltip title={description}>
-                <img src={imgUrl} style={{
-
-                    width: '30vh',
-                    height: '30vh',
-                    objectFit: 'cover',
-                    justifyContent: 'center',
-                }}/>
-
-            </Tooltip>
-            <h1 style={{color: "black",  alignItems:"center", fontSize: '30px',textAlign: 'center',}}>{label}</h1>
-        </div>
-
-    );
-
-
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  description,
+  imgUrl,
+  label,
+}) => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Tooltip title={description}>
+        <img
+          src={imgUrl}
+          style={{
+            width: "30vh",
+            height: "30vh",
+            objectFit: "cover",
+            justifyContent: "center",
+          }}
+        />
+      </Tooltip>
+      <h1
+        style={{
+          color: "black",
+          alignItems: "center",
+          fontSize: "30px",
+          textAlign: "center",
+        }}
+      >
+        {label}
+      </h1>
+    </div>
+  );
 };
 export default ProjectCard;
