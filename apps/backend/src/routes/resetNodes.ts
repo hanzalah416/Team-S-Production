95d3d8426a38029f3b3d251f6e7482981733c56e
Hanzalah Qamar <hanzalah.qamar02@gmail.com>
2024-04-24 21:50:35 -0400

    fixed resetting nodes but it loads for a bit

--- apps/backend/src/routes/resetNodes.ts
+++ apps/backend/src/routes/resetNodes.ts
@@ -1,18 +1,21 @@
 import PrismaClient from "../bin/database-connection.ts";
-import express, { Router } from "express";
-
+import express, { Router, Request, Response } from "express";
 import seed from "../seed.ts";
 
 const router: Router = express.Router();
-router.post("/", async () => {
-  seed()
-    .then(async () => {
-      await PrismaClient.$disconnect();
-    })
-    .catch(async (e) => {
-      console.error(e);
-      await PrismaClient.$disconnect();
-    });
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    await seed();
+    await PrismaClient.$disconnect();
+    res.status(200).send({ message: "Reset completed successfully." });
+  } catch (error) {
+    console.error(error);
+    await PrismaClient.$disconnect();
+    // Assert that error is of type Error to access message property
+    const message = (error as Error).message;
+    res.status(500).send({ message: "Failed to reset data.", error: message });
+  }
 });
 
 export default router;
