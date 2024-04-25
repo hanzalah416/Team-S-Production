7da6a47071209e9a4f1546a4a80424193817b8ab
KimAllise32 <114198322+KimAllise32@users.noreply.github.com>
2024-04-24 23:52:02 -0400

    Revert "Put background on about page + styling changes"

--- apps/frontend/src/components/about_page/AboutPage.tsx
+++ apps/frontend/src/components/about_page/AboutPage.tsx
@@ -11,166 +11,116 @@ import ken from "../assets/abtPage/ken.png";
 import li from "../assets/abtPage/li.png";
 import Card from "./Card";
 import styles from "./AboutPage.module.css";
-import BackgroundImg2 from "../assets/blue-background2.jpg";
 const Project = [
   {
     description: "Full Stack Developer",
     imgUrl: dor,
     label: "Dorothy Alexander",
-    font: "Elia",
-    color: "#D0B136",
-    size: "30px",
   },
   {
     description: "Project Manager, Full Stack Developer",
     imgUrl: jacob,
     label: "Jacob Antepli",
-    font: "Elia",
-    color: "#292877",
-    size: "35px",
   },
   {
     description: "Lead Software Engineer, Full Stack Developer",
     imgUrl: matt,
     label: "Matt Crane",
-    font: "Elia",
-    color: "#D0B136",
-    size: "35px",
   },
   {
     description: "Assistant Lead Software Engineer, Full Stack Developer",
     imgUrl: kim,
     label: "Kim Cummings",
-    font: "Elia",
-    color: "#292877",
-    size: "35px",
   },
   {
     description: "Documentation Analyst, Full Stack Developer",
     imgUrl: chris,
     label: "Christopher Hunt",
-    font: "Elia",
-    color: "#292877",
-    size: "32px",
   },
   {
     description: "Full Stack Developer, Scrum Master",
     imgUrl: li,
     label: "Jeffrey Li",
-    font: "Elia",
-    color: "#D0B136",
-    size: "35px",
   },
   {
     description: "Full Stack Developer, Product Owner",
     imgUrl: jav,
     label: "Javier Moncada",
-    font: "Elia",
-    color: "#292877",
-    size: "35px",
   },
   {
     description: "Full Stack Developer",
     imgUrl: hanz,
     label: "Hanzalah Qamar",
-    font: "Elia",
-    color: "#D0B136",
-    size: "33px",
   },
 
   {
     description: "Assistant Lead Software Engineer, Full Stack Developer",
     imgUrl: nate,
     label: "Nathaniel Schneider",
-    font: "Elia",
-    color: "#D0B136",
-    size: "29px",
   },
   {
     description: "Full Stack Developer",
     imgUrl: ken,
     label: "Ken Sebastian",
-    font: "Elia",
-    color: "#292877",
-    size: "35px",
   },
   {
     description: "Full Stack Developer",
     imgUrl: went,
     label: "Wentao Yu",
-    font: "Elia",
-    color: "#D0B136",
-    size: "35px",
   },
 ];
 
 export const AboutPage = () => {
   return (
+    <section
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "30px",
+        paddingTop: "120px",
+        paddingBottom: "40px",
+      }}
+    >
       <div
-          style={{
-              backgroundImage: `url(${BackgroundImg2})`,
-              height: "100vh",
-              width: "100vw",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              minHeight: "100%",
-              backgroundPosition: "center center",
-              overflowX: "hidden",
-          }}
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "30px",
+        }}
       >
-          <section
-              style={{
-                  display: "flex",
-                  flexWrap: "wrap",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  gap: "30px",
-                  paddingTop: "120px",
-                  paddingBottom: "40px",
-              }}
-          >
-              <div
-                  style={{
-                      alignItems: "center",
-                      justifyContent: "center",
-                      gap: "30px",
-                  }}
-              >
-                  <h1 className={styles.heading}>Our Team</h1>
-              </div>
-
-              <div
-                  style={{
-                      display: "flex",
-                      flexWrap: "wrap",
-                      justifyContent: "center",
-                      gap: "35px",
-                  }}
-              >
-                  {Project.map((item) => (
-                      <div className={styles.pictureBackground}>
-                          <Card {...item} key={item.description}/>
-                      </div>
-                  ))}
-              </div>
-              <div>
-                  <h1 className={styles.secondary}>WPI Computer Science Department</h1>
-
-                  <h1 className={styles.secondary}>Professor Wilson Wong</h1>
-
-                  <h1 className={styles.secondary}>Team Coach: Youssef Benchikhi </h1>
-
-                  <p className={styles.iaintreadinallat}>
-                      We would like to thank Brigham and Women's Hospital and their
-                      representative Andrew Shinn
-                  </p>
-                  <p className={styles.text}>
-                      *The Brigham & Women’s Hospital maps and data used in this application
-                      are copyrighted and provided for the sole use of educational purposes.
-                  </p>
-              </div>
-        </section>
+        <h1 className={styles.heading}>Our Team</h1>
       </div>
-)
-    ;
+
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "35px",
+        }}
+      >
+        {Project.map((item) => (
+          <Card {...item} key={item.description} />
+        ))}
+      </div>
+      <div>
+        <h1 className={styles.secondary}>WPI Computer Science Department</h1>
+
+        <h1 className={styles.secondary}>Professor Wilson Wong</h1>
+
+        <h1 className={styles.secondary}>Team Coach: Youssef Benchikhi </h1>
+
+        <p className={styles.iaintreadinallat}>
+          We would like to thank Brigham and Women's Hospital and their
+          representative Andrew Shinn
+        </p>
+        <p className={styles.text}>
+          *The Brigham & Women’s Hospital maps and data used in this application
+          are copyrighted and provided for the sole use of educational purposes.
+        </p>
+      </div>
+    </section>
+  );
 };
--- apps/frontend/src/components/about_page/Card.tsx
+++ apps/frontend/src/components/about_page/Card.tsx
@@ -4,18 +4,12 @@ interface ProjectCardProps {
   description: string;
   imgUrl: string;
   label: string;
-  font: string;
-  color: string;
-  size: string;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
   description,
   imgUrl,
   label,
-  font,
-  color,
-  size,
 }) => {
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
@@ -33,27 +27,15 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
         </Tooltip>
         <h1
           style={{
-            width: "30vh",
-            height: "30vh",
-            objectFit: "cover",
-            marginLeft: "auto",
-            marginRight: "auto",
-            justifyContent: "center",
+            color: "black",
+            alignItems: "center",
+            fontSize: "20px",
+            textAlign: "center",
           }}
-        />
-      </Tooltip>
-        <br />
-      <h1
-        style={{
-          color: color,
-          fontFamily: font,
-          alignItems: "center",
-          fontSize: size,
-          textAlign: "center",
-        }}
-      >
-        {label}
-      </h1>
+        >
+          {label}
+        </h1>
+      </div>
     </div>
   );
 };
