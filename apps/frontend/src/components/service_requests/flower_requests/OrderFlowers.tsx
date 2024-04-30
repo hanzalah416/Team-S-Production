eca4ee73c3aba394b9485827aaa8b6a7cad7f742
Dorothy Alexander <dcalexander2004@gmail.com>
2024-04-29 23:25:05 -0400

    Precariously working carousels

--- apps/frontend/src/components/service_requests/flower_requests/OrderFlowers.tsx
+++ apps/frontend/src/components/service_requests/flower_requests/OrderFlowers.tsx
@@ -21,6 +21,9 @@ import Paper from "@mui/material/Paper";
 import BackgroundImg2 from "../../assets/blue-background2.jpg";
 import Tooltip from "../../ToolTip.tsx";
 import styles from "../../login/Login.module.css";
+// @ts-expect-error Problem with splides library
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css/skyblue";
 
 const tips = `
 Name of Requester: Enter the name of the person requesting the flowers.
@@ -322,27 +325,57 @@ const OrderFlowers: React.FC = () => {
                 Flower Type
               </InputLabel>
               <br />
-              <div style={{ display: "flex" }}>
-                <img
-                  src={poppies}
-                  alt="Covering 3/4 page"
-                  className={styles.poppies}
-                  style={{ width: "200px", height: "auto" }}
-                />
 
-                <img
-                  src={roses}
-                  alt="Covering 3/4 page"
-                  className={styles.roses}
-                  style={{ width: "200px", height: "auto" }}
-                />
-                <img
-                  src={tulips}
-                  alt="Covering 3/4 page"
-                  className={styles.tulips}
-                  style={{ width: "200px", height: "auto" }}
-                />
-              </div>
+
+                <Splide width={400}
+                    aria-label="Carousel of gifts"
+                    style={{
+                        width: "400px",
+                        maxWidth: "400px",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                    <SplideSlide>
+                        <img
+                            src={poppies}
+                            className={styles.poppies}
+                            alt="Poppies"
+                            style={{
+                                width: "300px",
+                                height: "auto",
+                                marginLeft: "auto",
+                                marginRight: "auto"
+                            }} />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img
+                            src={roses}
+                            className={styles.roses}
+                            alt="Roses"
+                            style={{
+                                width: "300px",
+                                height: "auto",
+                                marginLeft: "auto",
+                                marginRight: "auto"
+                            }} />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img
+                            src={tulips}
+                            className={styles.tulips}
+                            alt="Tulips"
+                            style={{
+                                width: "300px",
+                                height: "auto",
+                                marginLeft: "auto",
+                                marginRight: "auto"
+                            }} />
+                    </SplideSlide>
+                </Splide>
+
+
               <ToggleButtonGroup
                 color="primary"
                 value={typeFlower} // Use the state value here
--- apps/frontend/src/components/service_requests/gift_request/giftForm.tsx
+++ apps/frontend/src/components/service_requests/gift_request/giftForm.tsx
@@ -22,8 +22,9 @@ import fruit from "../../assets/GiftPhotos/fruitgift.png";
 import Tooltip from "../../ToolTip";
 import { Position } from "../../common/PositionInterface.ts";
 import { Node } from "../../common/NodeInterface.ts";
+// @ts-expect-error Problem with splides library
 import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
+import "@splidejs/react-splide/css/skyblue";
 
 const tips = `Name of Requester: Enter the full name of the person requiring language assistance.
 
@@ -53,71 +54,6 @@ export default function GiftForm() {
   const navigate = useNavigate(); //Function to navigate to other pages
   // const {getAccessTokenSilently} = useAuth0();
 
-    interface Desc {
-        desc: string;
-    }
-
-    interface Location {
-        location: string | number;
-    }
-
-    interface LocationDesc extends Location, Desc {}
-
-    const River: LocationDesc = {
-        location: 17,
-        desc: "This is an image I took during my time in Tokyo in 2019. It was taken after I just finished working on a large production for a major brand. I had a lot of fun shooting that day so this shot after work is always memorable to me. Somewhere in western Tokyo..",
-    };
-
-    const Train: LocationDesc = {
-        location: "Somewhere on the Yamanote Line, Tokyo",
-        desc: "This is an image I took on the train in Tokyo. It was taken on the Yamanote Line - one of the most popular train lines in Tokyo. It runs in a huge circle around Tokyo's most popular train stops and acts as lifeline for people to transfer to other lines. I don't think I was expecting this kid to rush out of the train when I was taking this photo but it works!",
-    };
-
-    const Ral: LocationDesc = {
-        location: "Shibuya Game Center, Tokyo",
-        desc: "This is an image I took in a game center in Tokyo. I was shooting a lookbook for a brand and thought it would be a fun location. Game centers in Japan are a sensory overload, there are plenty of different kinds of games to play. I had just beaten my friend Ral, the model at Mario Kart just after taking this photo.",
-    };
-
-    const Denwa: LocationDesc = {
-        location: "Mishima station, Shizouka",
-        desc: "I took this photo on a trip to visit my host family in the countryside. There are so many nostagalic places and things in Japan, but this stood out to me after I arrived at their local station - several hours away from Tokyo, by the base of Mt. Fuji.",
-    };
-
-    const Hana: LocationDesc = {
-        location: "Shibuya Underpass, Tokyo",
-        desc: " I took this photo with some friends in Shibuya under an overpass. My friend owns a flower shop and after a long day of shooting we decieded to have some fun and burn these to get some cool images. Needless to say we got plenty of weird looks.",
-    };
-
-    const Kohei: LocationDesc = {
-        location: "Toyosu Bridge, Tokyo",
-        desc: " This was from that earlier shoot for my friends flower shop. He makes custom arranged flowers for fashion brands, weddings, and events. We were shooting a collaboration between his shop and another brand. This was a really cool location because it reminded me of skylines in any older anime and had a moody feel. Featured one of my best friends, Kohei.",
-    };
-
-    //Comment this out to use the interface portion of the code//
-    //Type Guard//
-    let x: Location = {
-        location: 10,
-    };
-
-    let y: Desc = {
-        desc: "This is an image I took during my time in Tokyo in 2019. It was taken after I just finished working on a large production for a major brand. I had a lot of fun shooting that day so this shot after work is always memorable to me. Somewhere in western Tokyo..",
-    };
-
-    function locationF(): string {
-        if (typeof x.location === "string") {
-            return x.location;
-        } else {
-            return "Location Unknown";
-        }
-    }
-
-    function descF(): string {
-        if (typeof y.desc === "string") {
-            return y.desc;
-        } else {
-            return "No Description Available";
-        }
-    }
 
 
   const toggleScrolling = (disableScroll: boolean) => {
@@ -362,99 +298,52 @@ export default function GiftForm() {
 
 
 
-                <Splide aria-label="My Favorite Images">
-                    <SplideSlide>
-                        <div className="img__wrap">
-                            <img className="img__img" src={river} alt="1" />
-                            <div className="img__desc">
-                                <p>{descF()}</p>
-                                <br />
-                                <br />
-                                <p>Location: {locationF()}</p>
-                            </div>
-                        </div>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <div className="img__wrap">
-                            <img className="img__img" src={train} alt="2" />
-                            <div className="img__desc">
-                                <p>{Train.desc}</p>
-                                <br />
-                                <br />
-                                <p>Location: {Train.location}</p>
-                            </div>{" "}
-                        </div>
-                    </SplideSlide>
-                    <SplideSlide>
-                        <div className="img__wrap">
-                            <img className="img__img" src={ral} alt="3" />
-                            <div className="img__desc">
-                                <p>{Ral.desc}</p>
-                                <br />
-                                <br />
-                                <p>Location: {Ral.location}</p>
-                            </div>{" "}
-                        </div>{" "}
-                    </SplideSlide>
-                    <SplideSlide>
-                        <div className="img__wrap">
-                            <img className="img__img" src={denwa} alt="4" />
-                            <div className="img__desc">
-                                <p>{Denwa.desc}</p>
-                                <br />
-                                <br />
-                                <p>Location: {Denwa.location}</p>
-                            </div>{" "}
-                        </div>{" "}
-                    </SplideSlide>
-                    <SplideSlide>
-                        <div className="img__wrap">
-                            <img className="img__img" src={hana} alt="5" />
-                            <div className="img__desc">
-                                <p>{Hana.desc}</p>
-                                <br />
-                                <br />
-                                <p>Location: {Hana.location}</p>
-                            </div>{" "}
-                        </div>{" "}
-                    </SplideSlide>
-                    <SplideSlide>
-                        <div className="img__wrap">
-                            <img className="img__img" src={kohei} alt="6" />
-                            <div className="img__desc">
-                                <p>{Kohei.desc}</p>
-                                <br />
-                                <br />
-                                <p>Location: {Kohei.location}</p>
-                            </div>{" "}
-                        </div>{" "}
-                    </SplideSlide>
-                </Splide>{" "}
+                <Splide
+                    aria-label="Carousel of gifts"
+                        style={{
+                            width: "400px",
+                            marginLeft: "auto",
+                            marginRight: "auto"
+                }}>
+                        <SplideSlide>
+                            <img
+                                 src={teddybear}
+                                 className={styles.poppies}
+                                 alt="Teddy bear"
+                                 style={{
+                                     width: "300px",
+                                     height: "auto",
+                                     marginLeft: "auto",
+                                     marginRight: "auto"
+                            }} />
+                        </SplideSlide>
+                        <SplideSlide>
+                            <img
+                                 src={fruit}
+                                 className={styles.roses}
+                                 alt="Chocolate covered strawberries"
+                                 style={{
+                                     width: "300px",
+                                     height: "auto",
+                                     marginLeft: "auto",
+                                     marginRight: "auto"
+                            }} />
+                        </SplideSlide>
+                        <SplideSlide>
+                            <img
+                                 src={coloringbook}
+                                 className={styles.tulips}
+                                 alt="Coloring Book"
+                                 style={{
+                                     width: "300px",
+                                     height: "auto",
+                                     marginLeft: "auto",
+                                     marginRight: "auto"
+                            }} />
+                        </SplideSlide>
+                </Splide>
 
 
-
-
-              <div style={{ display: "flex" }}>
-                <img
-                  src={coloringbook}
-                  alt="Covering 3/4 page"
-                  className={styles.poppies}
-                  style={{ width: "200px", height: "auto" }}
-                />
-
-                <img
-                  src={fruit}
-                  alt="Covering 3/4 page"
-                  className={styles.roses}
-                  style={{ width: "200px", height: "auto" }}
-                />
-                <img
-                  src={teddybear}
-                  alt="Covering 3/4 page"
-                  className={styles.tulips}
-                  style={{ width: "200px", height: "auto" }}
-                />
-              </div>
               <ToggleButtonGroup
                 color="primary"
                 value={typeGift} // Use the state value here
